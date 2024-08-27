d3f9963201d8107af81dd1d3089fcc626fb1254e
b4barnett <b4barnett@googlemail.com>
2024-08-27 14:53:09 +0100

    X for Y impl

--- Checkout.Core/PricingStrategy/ItemPriceStrategy.cs
+++ Checkout.Core/PricingStrategy/ItemPriceStrategy.cs
@@ -19,10 +19,19 @@ public class ItemPriceStrategy : IPricingStrategy
     public int GetTotalPrice( IGrouping<string, Item> basket ) => basket.Sum( x => x.Cost );
 }
 
-public class BuyXForYPricingStrategy(int NumberOfItems, int Amount) : IPricingStrategy
+public class BuyXForYPricingStrategy(int NumberOfItemsInBundle, int Amount) : IPricingStrategy
 {
     public int GetTotalPrice( IGrouping<string, Item> basket )
     {
-        throw new NotImplementedException();
+        if ( basket.Count() == 0 )
+        {
+            return 0;
+        }
+        int bundles = basket.Count() / NumberOfItemsInBundle;
+        int bundleCost = bundles * Amount;
+        int remainder = basket.Count() % NumberOfItemsInBundle;
+        int remainderCost = remainder * basket.First().Cost;
+
+        return bundleCost + remainderCost;
     }
 }
